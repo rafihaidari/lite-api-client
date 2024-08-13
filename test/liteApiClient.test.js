e8040f2bc86467fed7b52241676adf9a7f5616a0
Rafi Haidari <haidari.1988@gmail.com>
2024-08-13 17:30:37 +0200

    test packages removed.

--- test/liteApiClient.test.js
+++ test/liteApiClient.test.js
@@ -1,35 +1,35 @@
-// test/LiteApiClient.test.js
+// // test/LiteApiClient.test.js
 
-const LiteApiClient = require('../src/liteApiClient');
-const axios = require('axios');
-const MockAdapter = require('axios-mock-adapter');
+// const LiteApiClient = require('../src/liteApiClient');
+// const axios = require('axios');
+// const MockAdapter = require('axios-mock-adapter');
 
-// Create a mock adapter instance for axios
-const mock = new MockAdapter(axios);
+// // Create a mock adapter instance for axios
+// const mock = new MockAdapter(axios);
 
-describe('LiteApiClient', () => {
-  let api;
+// describe('LiteApiClient', () => {
+//   let api;
 
-  beforeEach(() => {
-    api = new LiteApiClient('https://api.example.com');
-  });
+//   beforeEach(() => {
+//     api = new LiteApiClient('https://api.example.com');
+//   });
 
-  it('should make a GET request', async () => {
-    const mockData = { id: 123, name: 'John Doe' };
-    mock.onGet('/users').reply(200, mockData);
+//   it('should make a GET request', async () => {
+//     const mockData = { id: 123, name: 'John Doe' };
+//     mock.onGet('/users').reply(200, mockData);
 
-    const response = await api.get('/users');
-    expect(response).toEqual(mockData);
-  });
+//     const response = await api.get('/users');
+//     expect(response).toEqual(mockData);
+//   });
 
-  it('should make a POST request', async () => {
-    const mockData = { id: 124, name: 'Jane Doe' };
-    const postData = { name: 'Jane Doe' };
-    mock.onPost('/users', postData).reply(201, mockData);
+//   it('should make a POST request', async () => {
+//     const mockData = { id: 124, name: 'Jane Doe' };
+//     const postData = { name: 'Jane Doe' };
+//     mock.onPost('/users', postData).reply(201, mockData);
 
-    const response = await api.post('/users', postData);
-    expect(response).toEqual(mockData);
-  });
+//     const response = await api.post('/users', postData);
+//     expect(response).toEqual(mockData);
+//   });
 
-  // Add more tests as needed for other methods and scenarios
-});
+//   // Add more tests as needed for other methods and scenarios
+// });
